cc8b81ec7db9dfc4a0989f3958c1c4ea75e7e46c
kimts2627 <kimtstststs@gmail.com>
2021-02-14 20:38:31 +0900

    Add window innerWidth module

--- src/modules/main.ts
+++ src/modules/main.ts
@@ -3,6 +3,7 @@ const CHANGE_CURRENT_PROJECT = 'main/CHANGE_CURRENT_PROJECT' as const;
 const HANDLE_DEGREE = 'main/HANDLE_DEGREE' as const;
 const HANDLE_CONTACT = 'main/HANDLE_CONTACT' as const;
 const HANDLE_MOVE_PAGE = 'main/HANDLE_MOVE_PAGE' as const;
+const HANDLE_WIDTH = 'main/HANDLE_WIDTH' as const;
 
 // 액션 생성 함수
 export const changeCurrentProject = () => ({ type: CHANGE_CURRENT_PROJECT });
@@ -14,11 +15,18 @@ export const handleDegree = (newDegree: number) => ({
 });
 export const handleContact = () => ({ type: HANDLE_CONTACT });
 export const handleMovePage = () => ({ type: HANDLE_MOVE_PAGE });
+export const handleWidth = (width: number) => ({
+  type: HANDLE_WIDTH,
+  payload: {
+    width
+  }
+});
 
 // 액션 타입
 type MainAction =
   | ReturnType<typeof changeCurrentProject> | ReturnType<typeof handleDegree>
   | ReturnType<typeof handleContact> | ReturnType<typeof handleMovePage>
+  | ReturnType<typeof handleWidth>
 
 // 스테이트 초기값
 interface MainState {
@@ -26,13 +34,15 @@ interface MainState {
   degree: number;
   isContactOn: boolean;
   isMain: boolean;
+  width: number;
 }
 
 const initialState: MainState = {
   currentProject: '',
   degree: 270,
   isContactOn: false,
-  isMain: true
+  isMain: true,
+  width: 0
 }
 
 // 리듀서
@@ -48,6 +58,8 @@ const main = (state = initialState, action: MainAction) => {
       return Object.assign({}, state, { isContactOn: !state.isContactOn })
     case HANDLE_MOVE_PAGE:
       return Object.assign({}, state, { isMain: !state.isMain })
+    case HANDLE_WIDTH:
+      return Object.assign({}, state, { width: action.payload.width })
     default:
       return state;
   }
